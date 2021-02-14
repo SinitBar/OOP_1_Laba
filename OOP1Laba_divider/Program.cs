e70fdca0299c881bf9e878a23300a3bf4c1a7ee1
SinBar <varka.sinitsckaya@yandex.ru>
2021-02-15 01:35:58 +0300

    Add files via upload

---
+++ OOP1Laba_divider/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP1Laba_divider
+{
+    class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            string temp;
+            Console.WriteLine("Please, enter first integer number");
+            try
+            {
+                i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please, enter second integer number");
+                temp = Console.ReadLine();
+                if (Int32.TryParse(temp, out j))
+                {
+                    try
+                    {
+                        int k = i / j;
+                        Console.WriteLine("The result of dividing first on second = {0}", k);
+                    }
+                    catch (Exception divide_on_zero)
+                    {
+                        if (j == 0)
+                            Console.WriteLine("Exception of dividing on zero: {0}", divide_on_zero.StackTrace);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entered string (second integer number) could not be converted to integer");
+                    Console.WriteLine("Сannot calculate the result of division");
+                }
+            }
+            catch (Exception wrong_format)
+            {
+                Console.WriteLine("Exception of wrong format of the number: {0}", wrong_format.StackTrace);
+            }
+        }
+    }
+}
---
+++ OOP1Laba_greeting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP1Laba_greeting
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string my_name;
+            Console.WriteLine("Please, write your name:");
+            my_name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}", my_name);
+        }
+    }
+}
